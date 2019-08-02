6247148f7df94e1a5ba94e541a6d42397ac79a48
zidol <ajh3166@naver.com>
2019-08-02 23:02:06 +0900

    ExceptionHandler 실습

---
+++ src/main/java/me/zidol/springbootexception/SampleException.java
@@ -0,0 +1,4 @@
+package me.zidol.springbootexception;
+
+public class SampleException extends RuntimeException {
+}
---
+++ src/test/java/me/zidol/springbootexception/SpringbootexceptionApplicationTests.java
@@ -0,0 +1,16 @@
+package me.zidol.springbootexception;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootexceptionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
